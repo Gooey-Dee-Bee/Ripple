2496b55d93defe19ebe27e5e792f3563dd890a1b
jrutz <jrutz@smu.edu>
2015-03-20 12:51:20 -0500

    Added pre-pending
    Because everyone wants to see their songs at the top and stuff

--- js/drop.js
+++ js/drop.js
@@ -1,7 +1,7 @@
 var beginPlayer = '<div class="songPlayer"> <div class="songText"><iframe src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/';
 var endPlayer ='"></iframe></div><img class="drop" src="images/dropit.png"/></div>'
 
-var newSongId;
+
 var songIdList = new Array();
 songIdList.push("173752179");
 songIdList.push("188883966");
@@ -15,18 +15,21 @@ function grabSong() {
 
 
 function addSong(songId) {
-	console.log("Add song"+ songId);
-	
-	var mydiv = document.getElementById("songBox");
     var newcontent = document.createElement('div');
     newcontent.innerHTML = beginPlayer+songId+endPlayer;
 
-    while (newcontent.firstChild) {
-        mydiv.appendChild(newcontent.firstChild);
-    }
-	//document.getElementById("songBox").innerHTML = value;
-	console.log(document.getElementById("songBox").innerHTML);
+    prependElement('songBox', newcontent);
+
 	console.log("SONG ADDED");
+	document.getElementById("songId").value="";
+}
+
+function prependElement(parentID, child){
+	parent = document.getElementById(parentID);
+	console.log(parent);
+	console.log(child);
+	parent.insertBefore(child, parent.childNodes[0]);
+
 }
 
 
@@ -43,39 +46,27 @@ function loadSongs() {
 
     	while (newcontent.firstChild) {
         	mydiv.appendChild(newcontent.firstChild);
-    	}
-    	/*
-		document.getElementById("songBox").innerHTML = value;
-		console.log(document.getElementById("song").innerHTML);*/
-	
-	
+    	}	
 	}
-
-
-
 }
 
 
 function getSongId(url) {
-
 var Request = new XMLHttpRequest();
 Request.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     console.log('Status:', this.status);
     console.log('Headers:', this.getAllResponseHeaders());
     console.log('Body:', this.responseText);     
+    
     var textin = JSON.parse(this.responseText);
-	console.log(textin);
-	newSongId = parseSong(textin);
-	console.log("getsongID songID = "+newSongId);
+	var newSongId = parseSong(textin);
 	addSong(newSongId);
-
   }
 }
+
 Request.open('GET', 'https://api.soundcloud.com/resolve.json?url='+url+'&client_id=dafab2de81f874d25715f0e225e7c71a', true);
 Request.send(JSON.stringify(document.body));
-
-
 }
 
 
