3820a44be4185ee9228fc01cd77da8f7d6e4be60
jstumbaugh <jstumbaugh@smu.edu>
2015-03-22 18:08:28 -0500

    the struggle with the login post is way too real

--- website/js/landingpage.js
+++ website/js/landingpage.js
@@ -1,46 +1,31 @@
 $( document ).ready(function() {
 
 	/* Login Function */
-	$("#loginForm").submit(function(event) {
+	$("#loginForm").submit( function(event) {
 		event.preventDefault();
 		var user = new Object();
 		user.email = $("#loginEmail").val();
 		user.password = $("#loginPassword").val();
 		// user now has email and password stored in it
 
-
-		$.ajax({
-			type: "POST",
-		    url: 'php/login.php',
-		    content: 'application/json',
-		    data: JSON.stringify(user),
-		    success: function(data){
-		        //Error Checking
-		        // if($.isNumeric(data)){
-		        //     if(data==400) {
-		        //         alert("success logging in");
-		        //     } 
-		        //     else {
-		                
-		        //     } 
-		        // }
-		        // else if(!jQuery.isEmptyObject(data)){
-		        //     var obj = JSON.parse(data);
-		        //     if(obj.Email.length>0){
-		        //         $.cookie.json = true;
-		        //         $.cookie("data", data); 
-		        //         //redirect user
-		        //         $("#loginMessage").hide();
-		        //         $(location).attr('href', "search.html");
-		        //     }
-		        // }
-		    }
-		}); // end of ajax
+		// this url returns a json string of { "password": "password" }
+		$.get("http://private-17c6-rippleapi.apiary-mock.com/users/email", function(data, status) {
+			console.log("Data: " + data);
+			/*var obj = JSON.parse(data);
+			console.log("hasdlfkhasdf");
+			if (user.password === obj.password) {
+	        	alert("success!!");
+	        	// redirect to home page
+	        }
+	        else {
+	        	alert("wrong password!");
+	        }*/
+		});
 	}); // end of login function
 
 
 	/* Sign Up Function */
-	$("#signUpForm").submit( function(event) {
+	/*$("#signUpForm").submit( function(event) {
 		event.preventDefault();
 		var user = new Object();
 		user.email = $("#signupEmail").val();
@@ -81,6 +66,6 @@ $( document ).ready(function() {
 		// }
 
 	}; // end of sign up
-
+	*/
 }); // end of document ready func
 
