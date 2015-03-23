61016e42039ed4ea3fa18dc80a57fb7ebb513c32
jstumbaugh <jstumbaugh@smu.edu>
2015-03-23 14:41:43 -0500

    Login and Create Account now work!!!
    The work with the custom Apiary’s, not the DB just yet. There is also
    no redirection from the landing page to the homepage

--- website/js/landingpage.js
+++ website/js/landingpage.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$( document ).ready( function() {
 
 	/* Login Function */
 	$("#loginForm").submit( function(event) {
@@ -10,33 +10,54 @@ $( document ).ready(function() {
 
 		$.ajax({
 			type: "POST",
-		    url: 'http://private-17c6-rippleapi.apiary-mock.com/users/email',
+		    url: 'http://private-f89294-ripple3.apiary-mock.com/users/email',
 		    content: 'application/json',
 		    data: JSON.stringify(user),
-		    success: function(data) {
-		    	console.log(data);
-		    	/*if(data === "Successfully Logged In")
+		    success: function(data, status, request) {
+		    	if(data === "Successfully Logged In")
 		    		alert("Successfully Logged In");
 		    	else
-		    		alert("Account not found.");*/
-		    }
+		    		alert("Account not found.");
+		    },
+		    error: function(something, var1) {
+		    	console.log(something);
+		    	console.log(var1);
+         		alert('An error occurred');
+      		}
 		}); // end of ajax
-
 	}); // end of login function
 
+	
 
 	/* Sign Up Function */
 	$("#signUpForm").submit( function(event) {
 		event.preventDefault();
 		var user = new Object();
-		user.email = $("#signupEmail").val();
-		user.password = $("#signupPassword").val();
-		user.confirmPassword = $("#signupConfirmPassword").val();
-		if (user.password != user.confirmPassword) {
-			html = "*Passwords are not the same.";
+		user.email = $(".signupEmail").val();
+		user.password = $(".signupPassword").val();
+		var confirmPassword = $(".signupConfirmPassword").val();
+		if ( user.password == confirmPassword ) {
+			$.ajax({
+				type: "POST",
+			    url: 'http://private-f89294-ripple3.apiary-mock.com/newuser/email',
+			    content: 'application/json',
+			    data: JSON.stringify(user),
+			    success: function(data, status, request) {
+			    	if(data === "success")
+			    		alert("Successfully Created Account");
+			    	else
+			    		alert("Account already exists");
+			    },
+			    error: function(something, var1) {
+			    	alert('An error occurred creating the account');
+	      		}
+			}); // end of ajax
+		} // end of if
+		else {
+			html = "Passwords are not the same.";
 			document.getElementById("errorMessage").innerHTML = html;
-			return;
-		}
-	}; // end of sign up
+		} // end of else
+	}); // end of sign up
+	
 }); // end of document ready func
 
