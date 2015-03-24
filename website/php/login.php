487bafc4a6c05d84dd730bc23c951ed13657975d
averyferrante <aferrante@smu.edu>
2015-03-24 12:32:13 -0500

    Working login and account creation.

--- website/php/login.php
+++ website/php/login.php
@@ -1,15 +1,5 @@
 <?php
-	function getInfoFromDatabase($query) {
-		$con = mysqli_connect('localhost', 'root', 'mysqlpassword', 'Ripple');
-		if (!$con) {
-    		die('Could not connect: ' . mysqli_error($con));
-		}
-
-		$result = mysqli_query($con, $query);
-		mysqli_close($con);
-
-		return $result; // This will return FALSE if the query fails
-	}
+	session_start();
 
 	// DECODE THE INFORMATION (IT IS PASSED IN JSON FORMAT)
 	$post_json = file_get_contents("php://input");
@@ -25,10 +15,26 @@
 	$result = $result['pword'];
 
 	if($result == $pword) {
+		$_SESSION['email'] = $email; // Account exists, set the session variables.
+		$_SESSION['password'] = $pword;
 		echo "Successfully Logged In";
 	}
 	else {
 		echo "Account Not Found";
+	}
+
+
+
+	function getInfoFromDatabase($query) {
+		$con = mysqli_connect('localhost', 'root', 'mysqlpassword', 'Ripple');
+		if (!$con) {
+			die('Could not connect: ' . mysqli_error($con));
+		}
+
+		$result = mysqli_query($con, $query);
+		mysqli_close($con);
+
+		return $result; 
 	}
 
 ?>
