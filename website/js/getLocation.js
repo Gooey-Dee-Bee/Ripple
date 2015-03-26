adcd9583423a825959c3248173f669ae8d18572c
jrutz <jrutz@smu.edu>
2015-03-26 18:06:23 -0500

    Displaying geo info upon valid zip submission

--- website/js/getLocation.js
+++ website/js/getLocation.js
@@ -11,23 +11,36 @@ $(document).ready(function(){
 	    
 	    document.getElementById("location").style.display = 'none';
 		document.getElementById("dropBox").style.display = 'block';
+		document.getElementById("songBox").style.display = 'block';
 		
 	    console.log("User city: " + visitor_city);
 	}
 	else
 	{
-		$('#location').append("<form id='location form'><label>Zip Code: </label><input class='locationInput' type='text'/><br/><input type='button' id='locationSubmit' value='SUBMIT' onClick='disappearZip()'/></form>");
+		$('#location').append("<form id='location form'><label>Zip Code: </label><input required='required' class='locationInput' type='text' id='zipcode'/><br/><input type='button' id='locationSubmit' value='SUBMIT' onClick='disappearZip()'/></form>");
 	    console.log("Error Getting IP Address");
 	}
 });
 
 function disappearZip () {
 
+	
+	if(isValidUSZip(document.getElementById("zipcode").value) == true)
+	{
 	//Call to push zipcode into the database?
-	document.getElementById("location").style.display = 'none';
-	document.getElementById("dropBox").style.display = 'block';
+		document.getElementById("location").style.display = 'none';
+		document.getElementById("dropBox").style.display = 'block';
+		document.getElementById("songBox").style.display = 'block';
+	} else {
+			alert("If you're a living person, you have a zipcode. Please enter it correctly.");
+	}
 
 
 
 
 }
+
+
+function isValidUSZip(sZip) {
+   		return /^\d{5}(-\d{4})?$/.test(sZip);
+	}
