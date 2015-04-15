0553596f833af4b943bb95d5171abec084e9da80
jstumbaugh <jstumbaugh@smu.edu>
2015-04-15 13:50:40 -0500

    testing it

--- website/js/search.js
+++ website/js/search.js
@@ -2,5 +2,8 @@ SC.initialize({
   client_id: 'dafab2de81f874d25715f0e225e7c71a'
 });
 
-
-
+var titleEntered = "Stay With Me" ; 
+console.log("SEARCH.JS");
+SC.get('/tracks', { title: titleEntered }, function(tracks) {
+  console.log(tracks);
+});
