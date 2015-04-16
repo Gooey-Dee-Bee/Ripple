36a1ca2f994fb49fa26eb9846cb541f621901341
jstumbaugh <jstumbaugh@smu.edu>
2015-04-16 09:14:41 -0500

    still not working just yet

--- website/js/search.js
+++ website/js/search.js
@@ -1,9 +1,30 @@
 SC.initialize({
-  client_id: 'dafab2de81f874d25715f0e225e7c71a'
+	client_id: 'dafab2de81f874d25715f0e225e7c71a'
 });
 
-var titleEntered = "Stay With Me" ; 
+var query = "Stay With Me";
 console.log("SEARCH.JS");
-SC.get('/tracks', { title: titleEntered }, function(tracks) {
-  console.log(tracks);
-});
+// SC.get('/tracks', { q: query }, function(tracks) {
+// 	console.log(tracks);
+// });
+
+
+
+function showSongSearch() {
+	document.getElementById('searchBox').style.display = "block";
+	document.getElementById('dropBox').style.display = "none";
+	document.getElementById('songSearchSuggest').style.display = "none";
+	document.getElementById('urlSearchSuggest').style.display = "block";
+	console.log('show Song Search');
+}
+
+function showURLSearch() {
+	document.getElementById('dropBox').style.display = "block";
+	document.getElementById('searchBox').style.display = "none";
+	document.getElementById('urlSearchSuggest').style.display = "none";
+	document.getElementById('songSearchSuggest').style.display = "block";
+	console.log('show URL Search');
+}
+
+//showSongSearch();
+showURLSearch();
