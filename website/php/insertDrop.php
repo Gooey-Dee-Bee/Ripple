7bcf82e1e7e8ce5071b71ab6e029e5aee85aa238
NMendoza <nmendoza@smu.edu>
2015-04-18 19:15:34 -0500

    Drop Geo in php

---
+++ website/php/insertDrop.php
@@ -0,0 +1,25 @@
+<?php
+	require_once(__DIR__."/databases.php"); // Allow access to the database functions
+	$id = $_POST['id']; // retieve the song ID
+	$email = $_POST['email'];	// retreive user email
+	$defaultPoints = 10;
+	$query = 'SELECT * FROM song WHERE song_id = $id';
+	if(!existsInDatabase($query)) {
+		$query = "INSERT INTO song VALUES($id, 1, 'filler', 'filler.com')"; // Lots of filler stuff that will hopefully be fixed
+		addToDatabase($query);
+		// Subtract DEFAULT value of points from user
+		$points = getInfoFromDatabase("SELECT points FROM users WHERE email = '$email'");
+		$points = mysqli_fetch_assoc($points);
+		$points = $points['points'];
+		$points = $points - $defaultPoints;
+		//echo "Points after substraction: $points";
+		if ($points < 0) {
+			$points = 0;
+		}
+		$sql = "UPDATE users SET points=$points WHERE email='$email'";
+		addToDatabase($sql);
+	}
+	$query = "INSERT INTO drops(user_id, song_id) VALUES(1, $id)";
+	addToDatabase($query);
+	echo $id;
+?>
