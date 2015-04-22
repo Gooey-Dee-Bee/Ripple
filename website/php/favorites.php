4a768cb604311b0a100be7733f6e2061b8108a14
CaitlinG <cgutschenrit@smu.edu>
2015-04-22 01:36:00 -0500

    rough favorites

--- website/php/favorites.php
+++ website/php/favorites.php
@@ -1,4 +1,20 @@
 <?php
+	require_once(__DIR__."/databases.php"); 
 
+	$post_json = file_get_contents("php://input");
+	$post = json_decode($post_json, true);
 
->
+	$userID = $_GET['user_id'];
+	$dropID = $_GET['dropID'];
+	$timestamp = CURRENT_TIMESTAMP;
+
+	$query = "SELECT * FROM users WHERE user_id = $userID AND drop_id = $dropID";
+	if(existsInDatabase($query)){
+		echo 200;
+	} else {
+		$add_query = "INSERT INTO favorites(user_id, drop_id) VALUES('$userID', '$dropID', 'timestamp')";
+		addToDatabase($add_query);
+		echo 100;
+	}
+
+?>
