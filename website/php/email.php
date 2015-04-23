c048ae24b7c42c43fed682140fc1cd6adc5f5c16
oklaiss <oklaiss@smu.edu>
2015-04-23 18:28:04 -0500

    working with email.php

--- website/php/email.php
+++ website/php/email.php
@@ -2,6 +2,16 @@
 	require_once(__DIR__."/dropHelper.php"); // Allow access to the drop functions
 	require_once(__DIR__."/databases.php"); // Allow access to the database functions
 
+	//generate key
+	//store it in new user and key database
+	//send email using .txt file replacing username and email
+	$email = $_POST['email']; // retieve the Email
 
+	//Create random key for this email
+	$key = $email . date('mY');	// concatenate email and date in xxXXXX format
+	$key = md5($key);	//md5 hash encryption
+
+	$confirm = addToDatabase("INSERT INTO confirm VALUES('$email','$key')");
+	
 
 ?>
