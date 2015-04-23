c7ebd8a97fe1e6d8a2334113462faf556baaba97
oklaiss <oklaiss@smu.edu>
2015-04-22 21:16:20 -0500

    Progress on pointTraceBack, needs location

--- website/php/pointTraceBack.php
+++ website/php/pointTraceBack.php
@@ -1,14 +1,33 @@
 <?php
 	require_once(__DIR__."/dropHelper.php"); // Allow access to the database functions
 
-	$song_id = $_POST['song_id']; // retieve the song ID
-	$email = $_POST['email'];	// retrieve user email
-	$latitude = $_POST['latitude'];		// retrieve latitude
-	$longitude = $_POST['longitude'];	// retrieve longitude
-
+	$pointCounter = 1;
 	//use while look to go up the user tree until previous drop id == null?
-	if(!sameUserDrop($email, $song_id)){
+	$user;
+	$drop;
+	$prev_drop;
 
+	function traceBack( $user_id, $drop_id, $prev_drop_id){
+		global $user, $drop, $prev_drop, $pointCounter;
+
+		$user = user_id;
+		$drop = $drop_id;
+		$prev_drop = $prev_drop_id;
+		$nextPrev = "SELECT prev_drop_id FROM drops WHERE drop_id = '$prev_drop'";
+
+		while (getInfoFromDatabase($nextPrev)!=0) {
+			//get info about previous drop from database
+			$prevDropUser = "SELECT user_id FROM drops WHERE drop_id = '$prev_drop'";
+			$prevDropUser = getInfoFromDatabase($prevDropUser);
+			//increase points by pointCounter for that user
+			$query = "UPDATE users SET points = points+'$pointCounter' WHERE user_id = '$prevDropUser'";
+			addToDatabase($query);
+			$pointCounter += 1;
+			//reset variables to be valid for the prevdrop and repeat
+			$drop = getInfoFromDatabase($nextPrev);
+			$query2 = "SELECT prev_drop_id FROM drops WHERE drop_id = '$drop'";
+			$prev_drop = getInfoFromDatabase($query2);
+		}
 	}
 
 ?>
