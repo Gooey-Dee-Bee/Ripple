06061d721db8495915686de663b3a1e42932f34f
averyferrante <aferrante@smu.edu>
2015-04-24 11:33:21 -0500

    Added a new table to the database to help with email confirmation.

--- website/database/data.sql
+++ website/database/data.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: Ripple
 -- ------------------------------------------------------
--- Server version	5.5.41-0ubuntu0.12.04.1
+-- Server version	5.5.43-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,30 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `confirmation`
+--
+
+DROP TABLE IF EXISTS `confirmation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `confirmation` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `confirm_key` varchar(60) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `confirmation`
+--
+
+LOCK TABLES `confirmation` WRITE;
+/*!40000 ALTER TABLE `confirmation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `confirmation` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `drops`
@@ -33,7 +57,7 @@ CREATE TABLE `drops` (
   PRIMARY KEY (`drop_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `drops_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +66,7 @@ CREATE TABLE `drops` (
 
 LOCK TABLES `drops` WRITE;
 /*!40000 ALTER TABLE `drops` DISABLE KEYS */;
-INSERT INTO `drops` VALUES (1,1,123063109,'2015-04-09 00:19:14',0,0,0),(2,1,146962699,'2015-04-09 00:20:50',0,0,0),(3,1,18853020,'2015-04-09 00:26:25',0,0,0);
+INSERT INTO `drops` VALUES (4,1,136949481,'2015-04-24 16:23:21',0,32.846,-96.7837),(5,6,18853020,'2015-04-24 16:29:27',0,32.846,-96.7837),(6,4,4138540,'2015-04-24 16:30:39',0,32.846,-96.7837);
 /*!40000 ALTER TABLE `drops` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,7 +94,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'password','aferrante@smu.edu',50,1),(2,'password','oklaiss@smu.edu',50,1),(3,'password','jrutz@smu.edu',50,1),(4,'password','nmendoza@smu.edu',50,1),(5,'new','new@new.com',50,1),(6,'password','master@smu.edu',999999,1);
+INSERT INTO `users` VALUES (1,'password','aferrante@smu.edu',40,1),(2,'password','oklaiss@smu.edu',50,1),(3,'password','jrutz@smu.edu',50,1),(4,'password','nmendoza@smu.edu',40,1),(5,'new','new@new.com',50,1),(6,'password','master@smu.edu',999989,1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -83,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-18 23:37:37
+-- Dump completed on 2015-04-24 16:31:47
