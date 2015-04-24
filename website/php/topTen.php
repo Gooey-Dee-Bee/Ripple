f5d20d092a9a8893b96cdf02105a342cc353a2e2
CaitlinG <cgutschenrit@smu.edu>
2015-04-24 16:44:17 -0500

    found the $

--- website/php/topTen.php
+++ website/php/topTen.php
@@ -1,5 +1,7 @@
 <?php
+
 	require_once(__DIR__."/databases.php");
+
 
 	$query = "SELECT song_id, count(song_id) as c
 				FROM drops
@@ -14,28 +16,14 @@
 	$i = 0;
 
 	foreach($result as $r){
-		$songID = r[$i]['song_id'];
-		$count = r[$i]['c'];
-		$rows[i] = array('songID' => $songID, 'count' => $count);
+		$songID = $r['song_id'];
+		$count = $r['c'];
+		$rows[$i] = array('songID' => $songID, 'count' => $count);
 
 		$i++;
 	}
 
-	/*while($r = mysqli_fetch_assoc($result)){
-
-		$rows[$i] = array(
-			'song_id' => $r['song_id'],
-			'count' => $r['count']
-			);
-
-		$i++;
-	}*/
-
 
 echo json_encode($rows);
-
-
-
-
 
 ?>
