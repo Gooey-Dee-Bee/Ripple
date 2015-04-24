ef767f4a95646cbefbabb68d9f4ffbbc1680a3b6
CaitlinG <cgutschenrit@smu.edu>
2015-04-24 13:47:13 -0500

    test

--- website/php/topTen.php
+++ website/php/topTen.php
@@ -1,10 +1,10 @@
 <?php
 	require_once(__DIR__."/databases.php");
 
-	$query = "SELECT song_id
+	$query = "SELECT song_id, count(song_id) as c
 				FROM drops
 				GROUP BY song_id
-				
+				ORDER BY c desc
 				limit 10";
 
 	$result = getInfoFromDatabase($query);
@@ -13,15 +13,23 @@
 
 	$i = 0;
 
-	while($r = mysqli_fetch_assoc($result)){
-
-		$rows[$i] = array(
-			'song_id' => $r['song_id'],
-			//'count' => $r['count']
-			);
+	foreach($result as $r){
+		$songID = r[$i]['song_id'];
+		$count = r[$i]['c'];
+		$rows[i] = array('songID' => $songID, 'count' => $count);
 
 		$i++;
 	}
+
+	/*while($r = mysqli_fetch_assoc($result)){
+
+		$rows[$i] = array(
+			'song_id' => $r['song_id'],
+			'count' => $r['count']
+			);
+
+		$i++;
+	}*/
 
 
 echo json_encode($rows);
